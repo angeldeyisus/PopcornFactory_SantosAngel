f39e0802272531a88f4049ea9be7075a7578330b
Angel Santos <angeldeyisus@users.noreply.github.com>
2025-02-17 22:34:45 -0700

    Se implementó la función de reserva de asientos

---
+++ app/src/main/java/santos/angel/popcornfactory_205750/Cliente.kt
@@ -0,0 +1,5 @@
+package santos.angel.popcornfactory_205750
+
+data class Cliente(var nombre:String,var tipoPago:String,
+              var asiento: Int) {
+}
--- app/src/main/java/santos/angel/popcornfactory_205750/DetallePelicula.kt
+++ app/src/main/java/santos/angel/popcornfactory_205750/DetallePelicula.kt
@@ -1,6 +1,8 @@
 package santos.angel.popcornfactory_205750
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -15,11 +17,26 @@ class DetallePelicula : AppCompatActivity() {
         setContentView(R.layout.activity_detalle_pelicula)
 
         val bundle = intent.extras
+        var nseats = 0
+        val seatLeft: TextView = findViewById(R.id.seatLeft)
 
         if (bundle != null) {
+             nseats = bundle.getInt("numberSeats")
+
             findViewById<TextView>(R.id.tv_nombre_pelicula).text = bundle.getString("nombre")
             findViewById<ImageView>(R.id.iv_pelicula_imagen).setImageResource(bundle.getInt("header"))
             findViewById<TextView>(R.id.tv_pelicula_desc).text = bundle.getString("sinopsis")
+            seatLeft.setText("$nseats seats available")
         }
+        if(nseats == 0){
+            findViewById<Button>(R.id.buyTickets).isEnabled = false
+        }else{
+            findViewById<Button>(R.id.buyTickets).isEnabled = true
+            findViewById<Button>(R.id.buyTickets).setOnClickListener{
+                var intent = Intent(this, SeatSelection::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
